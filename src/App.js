24a2cb007c2ab74956629b20dc6b75811a5dcc24
MD MAHADI HASAN <99705111+mahadiinsta@users.noreply.github.com>
2023-03-12 13:57:45 +0600

    added  new app functionality

--- src/App.js
+++ src/App.js
@@ -1,82 +1,114 @@
 // import SkillSection from "./SkillSection";
 import { useState } from "react";
 import "./App.css";
-import { Form } from "./components/Form";
-import { NoteList } from "./components/NoteList";
 
-function App() {
-  const [notes, setNotes] = useState([]);
-  const [noteTitle, setNoteTitle] = useState("");
+const App = () => {
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState([]);
   const [editMode, setEditMode] = useState(false);
-  const [editableMode, setEditableMode] = useState(null);
+  const [editableStudent, setEditableStudent] = useState(null);
+
+  const createStudentHandler = (e) => {
+    e.preventDefault();
+    if (!studentName) {
+      return alert ('please provide a valid name')
+    }
+    const newStudent = {
+      id : Date.now() + '',
+      name : studentName,
+      // isPresent : undefined    
+    }
+    setStudentName([...students,newStudent]);
+    setStudentName('')
+  };
+
+
+  const removeHandler = (id)=>{
+    // const newstudentList = students.filter((item)=>item.id !== id);
+    // setStudents(newstudentList);
+    setStudents(students.filter((item)=>item.id !== id));
+  }
+
+
+  const editHandler = (id) =>{
+    const toBeEditedStudent = students.find((item)=>item.id === id)
+    setEditMode(true)
+    setEditableStudent(toBeEditedStudent)
+    setStudentName(toBeEditedStudent.name)
+  }
+
+
+  const updateHandler = (e) =>{
+    e.preventDefault();
+    if (!studentName) {
+      return alert ('please provide a valid name')
+    }
+    const newstudentList = students.map((item) => {
+      if (item.id === editableStudent.id) {
+        item.name = studentName
+      }
+      return item
+    })
+    setStudents(newstudentList) 
+    setStudentName('')
+    setEditMode(false)
+    setEditableStudent(null)
+  }
 
   return (
-    <div className="App">
-      {/* <form
-        onSubmit={(e) => {
-          editMode ? updateHandler(e) : createHandler(e);
-        }}
-      >
+    <div>
+      <form onSubmit={(e)=>
+        editMode? updateHandler(e): createStudentHandler(e)} 
+        className="student-form">
         <input
-          value={noteTitle}
           type="text"
-          onChange={(event) => setNoteTitle(event.target.value)}
+          value={studentName}
+          onChange={(e) => setStudentName(e.target.value)}
         ></input>
-        <button
-          onClick={(e) => {
-            editMode ? updateHandler(e) : createHandler(e);
-          }}
-          type="submit"
-        >
-          {editMode ? "Update Now" : "Add Note"}
-        </button>
-      </form> */}
-      <Form
-        notes={notes}
-        noteTitle={noteTitle}
-        setNoteTitle={setNoteTitle}
-        setNotes={setNotes}
-        editableMode={editableMode}
-        setEditableMode={setEditableMode}
-        editMode={editMode}
-        setEditMode={setEditMode}
-      />
+        <button onClick={(e)=>
+        editMode?updateHandler(e):createStudentHandler(e)} 
+        type="submit">Add Student</button>
+      </form>
+      <div className="student-section">
+        <div className="list all-student-list">
+          <h2>All Student's list</h2>
+          <ul>
+            {students.map((student)=>(
+              <li>
+                <span>{student.name}</span>
+                <button onClick={() => editHandler(student.id)}>Edit</button>
+                <button onClick={() => removeHandler(student.id)}>Delete</button>
+                <button>Make Present</button>
+                <button>Make Absent</button>
 
-      {/* <ul>
-        {notes?.map((item) => (
-          <li key={item.id}>
-            <span>{item.title}</span>
-            <button onClick={() => editHandler(item.id)}>Edit</button>
-            <button onClick={() => removeHandler(item.id)}>Delete</button>
-          </li>
-        ))}
-      </ul> */}
-      <NoteList
-        setNoteTitle={setNoteTitle}
-        notes={notes}
-        setNotes={setNotes}
-        editableMode={editableMode}
-        setEditableMode={setEditableMode}
-        setEditMode={setEditMode}
-      />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="list present-student-list">
+          <h2>Present Student's list</h2>
+          <ul>
+            {students.filter((item)=>item.isPresent===true).map(student =>(
+              <li>
+                <span>{student.name}</span>
+                <button>Accidentally Added</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="list absent-student-list">
+          <h2>Absent Student's list</h2>
+          <ul>
+            {students.filter((item)=>item.isPresent===false).map(student =>(
+              <li>
+                <span>{student.name}</span>
+                <button>Accidentally Added</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
-
-// function App() {
-//   const mahirskills = ["js", "react", "node"];
-//   const fahimskills = ["js", "react", "node", "bootstrap"];
-//   const tazwerskills = ["js", "react", "node", "css", "wp"];
-//   return (
-//     <div className="App">
-//       <SkillSection skills={mahirskills} name="mahir" />
-//       <hr />
-//       <SkillSection skills={fahimskills} name="Fahim" />
-//       <hr />
-//       <SkillSection skills={tazwerskills} name="Tazwer" />
-//       <hr />
-//     </div>
-//   );
-// }
